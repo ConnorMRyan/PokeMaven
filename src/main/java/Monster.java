8522c73388efa9b1718bb03ead61ec93c80393a1
Connor Ryan <cryan13@student.cscc.edu>
2020-08-04 10:16:53 -0400

    First files, Start of the new project.

---
+++ src/main/java/Monster.java
@@ -0,0 +1,9 @@
+public class Monster {
+    private int HP; //Amount of health
+    private int ATK; // Physical attacking power
+    private int DEF; // Physical defensive power
+    private int SPC; // Special attack and defense
+    private int SPD; // Speed
+    private int typeOne; // Monster's primary type
+    private int typeTwo; // Optional secondary type
+}
---
+++ src/main/java/types.java
@@ -0,0 +1,19 @@
+public class types {
+    final public static int BUG = 1;
+    final public static int DRAGON = 2;
+    final public static int ICE = 3;
+    final public static int FIGHTING = 4;
+    final public static int FIRE = 5;
+    final public static int FLYING = 6;
+    final public static int GRASS = 7;
+    final public static int GHOST = 8;
+    final public static int GROUND = 9;
+    final public static int ELECTRIC = 10;
+    final public static int NORMAL = 11;
+    final public static int POISON = 12;
+    final public static int PSYCHIC = 13;
+    final public static int ROCK = 14;
+    final public static int WATER = 15;
+
+
+}
