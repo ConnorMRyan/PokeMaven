adb8e88f1de445bc81915e0bc0f475e0e73d4ca0
Connor Ryan <cryan13@student.cscc.edu>
2020-08-05 21:51:23 -0400

    Started to add DB connectivity

--- src/main/java/DatabaseConnection.java
+++ src/main/java/DatabaseConnection.java
@@ -1,9 +1,14 @@
 import java.sql.*;
 
 public class DatabaseConnection {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
+        try {
 
-        Connection db = DriverManager.getConnection("jdbc:postgresql://localhost/pokeMaven", "postgres", "password");
-        System.out.println("connected");
+            Connection db = DriverManager.getConnection("jdbc:postgresql://localhost/pokeMaven", "postgres", "password");
+        } catch (Exception e) {
+            System.err.println("Could not connect!");
+        }
     }
+
 }
+
