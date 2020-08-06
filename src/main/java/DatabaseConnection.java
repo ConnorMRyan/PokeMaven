52d3653c7fdeab3928e6ad88c73e526138afa56a
Connor Ryan <cryan13@student.cscc.edu>
2020-08-06 10:26:44 -0400

    Added dependency to pom

--- src/main/java/DatabaseConnection.java
+++ src/main/java/DatabaseConnection.java
@@ -3,7 +3,6 @@ import java.sql.*;
 public class DatabaseConnection {
     public static void main(String[] args) {
         try {
-
             Connection db = DriverManager.getConnection("jdbc:postgresql://localhost/pokeMaven", "postgres", "password");
         } catch (Exception e) {
             System.err.println("Could not connect!");
